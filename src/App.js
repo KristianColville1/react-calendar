c68edfa5ba582d9c98d7326e6149507a651ac562
KristianColville1 <kristiancolville1@gmail.com>
2023-04-12 19:50:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoList from './TodoList.js'
+
+function App() {
+  return (
+    <>
+    <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Todo</button>
+    </>
+  )
+}
+
+export default App;
